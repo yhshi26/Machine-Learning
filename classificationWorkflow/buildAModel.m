b59f4f787d03604d932e406c637a203350487057
Yosra Hashim <36962808+yhashim@users.noreply.github.com>
2021-07-16 16:42:27 -0400

    Update buildAModel.m

--- classificationWorkflow/buildAModel.m
+++ classificationWorkflow/buildAModel.m
@@ -1,5 +1,5 @@
-% A classification model is a partitioning of the space of predictor variables into regions. Each region is assigned one of the output classes. In this simple example with two 
-% predictor variables, you can visualize these regions in the plane.
+% A classification model is a partitioning of the space of predictor variables into regions. Each region is assigned one of the output classes. In this simple example with 
+% two predictor variables, you can visualize these regions in the plane.
 
 % Fit a Model
 % Instructions are in the task pane to the left. Complete and submit each task one at a time.
