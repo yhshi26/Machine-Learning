01bb1f5d714102c710ba53e8fac68b2e744da633
Yosra Hashim <36962808+yhashim@users.noreply.github.com>
2021-07-27 19:50:52 -0400

    Create trainingAModel.m

---
+++ classificationModels/trainingAModel.m
@@ -0,0 +1,13 @@
+% Try Different Classification Models
+% Instructions are in the task pane to the left. Complete and submit each task one at a time.
+
+% Do not edit. This code loads and plots the data.
+load letterdata.mat
+traindata
+
+% Task 1
+histogram(traindata.Character)
+
+% Task 2
+boxplot(traindata.MADX,traindata.Character)
+
